defc0755e77fc1baad56add268c8f3a7c3563541
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-28 19:00:45 -0300

    Next Ticket

--- controllers/socket.js
+++ controllers/socket.js
@@ -4,10 +4,12 @@ const ticketControl = new TicketControl;
 
 const socketController =  (socket)=>{
 
-socket.on('enviar-mensaje', (payload, callback) => {
-        const id = 123456; 
-        callback(id); 
-        socket.broadcast.emit('enviar-mensaje', payload); 
+socket.emit( 'ultimo-ticket', ticketControl.ultimo );
+
+socket.on('siguiente-ticket', (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente); 
+
     })
 }
 module.exports = {socketController}
