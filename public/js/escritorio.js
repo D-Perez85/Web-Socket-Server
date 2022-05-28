d4327786718f0a8fc1e8768d43a13da703a3cd8b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-28 19:12:19 -0300

    Screen Desktop

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,33 @@
 
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender    = document.querySelector('button');
+const lblTicket     = document.querySelector('small');
+const divAlerta     = document.querySelector('.alert');
+const lblPendientes = document.querySelector('#lblPendientes');
 
-console.log('Escritorio HTML');
+
+const searchParams = new URLSearchParams(window.location.search); 
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html'; 
+    throw new Error(' El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio'); 
+lblEscritorio.innerText = escritorio; 
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+btnAtender.addEventListener( 'click', () => {
+   
+});
