3ff3d34aff7e0e202a3921676b1535af053d4bbf
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-28 02:18:09 -0300

    Emit listener from the client

--- models/server.js
+++ models/server.js
@@ -27,10 +27,14 @@ class Server {
     sockets() {
 
         this.io.on('connection', socket=>{
-            console.log('Cliente conectado', socket.id);
+                console.log('Cliente conectado', socket.id);
 
             socket.on('disconnect', ()=>{
-            console.log('Cliente desconectado', socket.id);
+                console.log('Cliente desconectado', socket.id);
+            })
+
+            socket.on('enviar-mensaje', (payload)=>{
+                console.log(payload);
             })
         });
     }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,6 +1,8 @@
 // Referencias del HTML
 const lblOnline  = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar  = document.querySelector('#btnEnviar');
 
 const socket = io();
 
@@ -17,3 +19,15 @@ socket.on('disconnect', () => {
     lblOffline.style.display = '';
 });
 
+
+btnEnviar.addEventListener( 'click', () => {
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }   
+    socket.emit( 'enviar-mensaje', payload, ( id ) => {
+        console.log('Desde el server', id );
+    });
+});
