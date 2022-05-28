dfea8070a4d2c35a1332dce11c8c8d4b2588f86b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-28 02:04:37 -0300

    Socket front End

--- models/server.js
+++ models/server.js
@@ -7,15 +7,14 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.server = require('http').createServer(this.app);
-        this.io = require('socket.io')(this.server);
-        
-
-
+        this.io = require('socket.io')(this.server);        
         this.paths = {};
         // Middlewares
         this.middlewares();
         // Routes
         this.routes();
+        // Sockets
+        this.sockets();
     }
     middlewares() {
         // CORS
@@ -24,6 +23,16 @@ class Server {
         this.app.use( express.static('public') );
     }
     routes() {
+    }
+    sockets() {
+
+        this.io.on('connection', socket=>{
+            console.log('Cliente conectado', socket.id);
+
+            socket.on('disconnect', ()=>{
+            console.log('Cliente desconectado', socket.id);
+            })
+        });
     }
     listen() {
         this.server.listen( this.port, () => {
