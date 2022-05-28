44a4930469b4adfebe43b47258e8be9333c0aa00
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-28 01:30:48 -0300

    Initial Config Socket.io

--- models/server.js
+++ models/server.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 
 class Server {
     constructor(){
-        this.app   = express();
-        this.port  = process.env.PORT;
+        this.app = express();
+        this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+        
+
+
         this.paths = {};
         // Middlewares
         this.middlewares();
@@ -21,7 +26,7 @@ class Server {
     routes() {
     }
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Server active at port ', this.port );
         });
     }
